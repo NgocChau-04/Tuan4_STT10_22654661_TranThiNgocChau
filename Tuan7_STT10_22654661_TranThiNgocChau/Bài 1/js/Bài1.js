e770b0d1dff0303fb782a3c60776470b62466050
NgocChau-04 <ngocchau2004.mwg@gmail.com>
2024-10-28 21:18:57 +0700

    Delete Tuan7_STT10_22654661_TranThiNgocChau directory

--- Tuan7_STT10_22654661_TranThiNgocChau/Bài 1/js/Bài1.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    // Khi click vào hình ảnh, mở modal xem hình ảnh lớn
-    $('.img-fluid').on('click', function() {
-        var src = $(this).attr('src');
-        $('#galleryModal .modal-body img').attr('src', src);
-        $('#galleryModal').modal('show');
-    });
-});
